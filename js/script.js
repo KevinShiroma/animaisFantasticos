5db697cef59ece400c3393764e33de7152d4b43c
Kevin Shiroma <makoto.shiroma@hotmail.com>
2023-08-28 11:48:26 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/init-tab.js";
+import initModal from "./modules/modal.js";
 
 initScrollSuave();
 initAnimaScroll();
 initAccordion();
 initTabNav();
+initModal();
