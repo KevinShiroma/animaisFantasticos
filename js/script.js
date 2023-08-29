3c78bdd0d5d55a005147c3a0cc825dafa5aae04d
Kevin Shiroma <makoto.shiroma@hotmail.com>
2023-08-29 11:35:21 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ import initAnimaScroll from "./modules/anima-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/init-tab.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initMenuDropdown from "./modules/menu-dropdown.js";
 
 initScrollSuave();
 initAnimaScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
+initMenuDropdown();
