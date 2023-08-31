ad8a5eeaf6ef835057b5f28e168138008fdb48ef
Kevin Shiroma <makoto.shiroma@hotmail.com>
2023-08-31 11:28:35 -0300

    menu-hamburguer

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTabNav from "./modules/init-tab.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initMenuDropdown from "./modules/menu-dropdown.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initScrollSuave();
 initAnimaScroll();
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initTooltip();
 initMenuDropdown();
+initMenuMobile();
