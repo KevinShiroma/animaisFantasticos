2378f6739b877db1d41d304b632a6b347bd9c82f
Kevin Shiroma <makoto.shiroma@hotmail.com>
2023-09-21 10:19:36 -0300

    refatorar anima scroll

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import AnimacaoScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -38,7 +38,8 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 
-initAnimacaoScroll();
+const animaScroll = new AnimacaoScroll('[data-anime="scroll"]');
+animaScroll.init();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
